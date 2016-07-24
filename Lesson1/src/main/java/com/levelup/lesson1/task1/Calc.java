989b07f22bbd73ca659fec53e9acf3e16b7a8167
Arkadiy Dolgonos <arkad1y@ukr.net>
2016-07-24 18:40:26 +0300

    add task1 to lesson1

--- Lesson1/src/main/java/com/levelup/lesson1/task1/Calc.java
+++ Lesson1/src/main/java/com/levelup/lesson1/task1/Calc.java
@@ -4,4 +4,38 @@ package com.levelup.lesson1.task1;
  * Created by Алевтина on 24.07.2016.
  */
 public class Calc {
+    private int a;
+    private int b;
+
+    public Calc() {
+    }
+
+    public Calc(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public long sum() {
+        return a + b;
+    }
+
+    public double div() {
+        return (double) a / (double) b;
+    }
 }
